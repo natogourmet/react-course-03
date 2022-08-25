b04e978874d51b6ff8ea984e662c5f9d7946c39c
natogourmet <natogourmet@hotmail.com>
2022-08-25 00:34:43 -0500

    useState hook

--- src/CounterApp.tsx
+++ src/CounterApp.tsx
@@ -1,19 +1,31 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 interface Props {
   value: number;
 }
 
 export const CounterApp = ({ value }: Props) => {
+  const [count, setCount] = useState(value);
+
   const handleIncrement = (e: any) => {
-    value = 100;
-    console.log(e);
+    setCount((c) => c + 1);
+  };
+
+  const handleDecrement = (e: any) => {
+    setCount((c) => c - 1);
+  };
+
+  const handleReset = (e: any) => {
+    setCount(value);
   };
 
   return (
     <>
       <h1>Counter App</h1>
-      <h2>{value}</h2>
+      <h2>{count}</h2>
+      <button onClick={handleDecrement}>-1</button>
+      <button onClick={handleReset}>Reset</button>
       <button onClick={handleIncrement}>+1</button>
     </>
   );
