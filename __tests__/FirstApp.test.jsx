6e2ed7d1a20f5d3f8010180f58dbc3c605897495
natogourmet <natogourmet@hotmail.com>
2022-08-29 14:31:24 -0500

    component testing

--- __tests__/FirstApp.test.jsx
+++ __tests__/FirstApp.test.jsx
@@ -1,12 +1,22 @@
-import { render } from "@testing-library/react";
-import { FirstApp } from "../src/FirstApp";
+import { render } from '@testing-library/react';
+import { FirstApp } from '../src/FirstApp';
 
 describe('Tests on <FirstApp />', () => {
-  
   test('should match with snapshot', () => {
+    const name = 'Nato Gourmet';
+    const { container } = render(<FirstApp title={name} />);
+    expect(container).toMatchSnapshot();
+  });
 
-    render( <FirstApp /> );
+  test('should show title as an h1', () => {
+    const name = 'Nato Gourmet';
+    const { container, getByText } = render(<FirstApp title={name} />);
+
+    expect(getByText(name)).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+    expect(h1.innerHTML).toContain(name);
+
     
   });
-  
 });
